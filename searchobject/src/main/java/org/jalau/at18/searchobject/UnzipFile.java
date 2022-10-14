f58dbc5bf6442dc33274d3d62bd4f297e4da7320
Hugo Solares <Hugo.Solares@jala.university>
2022-10-14 11:43:55 -0400

    creating a class main to read and decompress zip files

--- searchobject/src/main/java/org/jalau/at18/searchobject/UnzipFile.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/UnzipFile.java
@@ -1,4 +1,4 @@
-package org.jalau.at18.searchobject.model;
+package org.jalau.at18.searchobject;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,8 +9,8 @@ import java.util.zip.ZipInputStream;
 
 public class UnzipFile {
     public static void main(String[] args) throws IOException {
-    String fileZip = "src/main/resources/unzipTest/compressed.zip";
-    File destDir = new File("src/main/resources/unzipTest");
+    String fileZip = "main/resources/unzipTest/compressed.zip";
+    File destDir = new File("main/resources/unzipTest");
     byte[] buffer = new byte[1024];
     ZipInputStream zis= new ZipInputStream(new FileInputStream(fileZip));
     ZipEntry zipEntry = zis.getNextEntry();
@@ -26,7 +26,6 @@ public class UnzipFile {
             if (!parent.isDirectory() && !parent.mkdirs()) {
                 throw new IOException("Failed to create directory " + parent);
             }
-
             // write file content
             FileOutputStream fos = new FileOutputStream(newFile);
             int len;
@@ -52,4 +51,42 @@ public class UnzipFile {
         }
         return destFile;
     }
+    
+    //another example
+    /*public static void main(String[] args) throws Exception {
+        if(args.length <2 ) {
+            throw new Exception("Insufficient no of parameters. Required-Parameter1: Full zip file name, Parameter2: Dest dir");
+
+        }
+        String zipFileName = "src/main/resources/unzipTest/compressed.zip";
+        String destDirectory = "src/main/resources/unzipTest";
+        File destDirectoryFolder = new File(destDirectory);
+        if (!destDirectoryFolder.exists()) {
+            destDirectoryFolder.mkdir();
+        }
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis= new ZipInputStream(new FileInputStream(zipFileName));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while(zipEntry !=null) {
+            String filePath = destDirectory + File.separator + zipEntry.getName();
+            System.out.println("Unzipping "+filePath);
+            if(!zipEntry.isDirectory()) {
+                FileOutputStream fos = new FileOutputStream(filePath);
+                int len;
+                while ((len = zis.read(buffer)) >0){
+                    fos.write(buffer,0,len);
+                }
+                fos.close();
+            }
+            else {
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zis.closeEntry();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+        System.out.println("Unzipping complete");
+    }*/
 }
