63dae3d1a4bdf8fc69abaf8e3c1cb2c1342efd06
Rodrigo Valda <Rodrigo.Valda@jala.university>
2022-10-20 12:09:02 -0400

    ZooModel integration and add new logic to merge with the MatchInfo

--- searchobject/src/main/java/org/jalau/at18/searchobject/modelrecognizer/ObjectDetection.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/modelrecognizer/ObjectDetection.java
@@ -5,15 +5,35 @@ import org.jalau.at18.searchobject.model.MatchInfo;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import ai.djl.ModelException;
+import ai.djl.translate.TranslateException;
+import org.jalau.at18.searchobject.model.objectDetection.ObjectDetectionModel;
+
+import java.io.IOException;
+import java.io.File;
 
 public class ObjectDetection implements ModelRecognizer {
+    ObjectDetectionModel object;
     @Override
     public List<MatchInfo> matching(Path pathFolder, String searchCriteria, int occurrencyPercentage) {
         List<MatchInfo> matches = new ArrayList<>();
-        matches.add(new MatchInfo("55.png", 82.9));
-        matches.add(new MatchInfo("67.png", 90.9));
-        matches.add(new MatchInfo("85.png", 85.9));
-        matches.add(new MatchInfo("180.png", 93.2));
+        try {
+
+            File file = pathFolder.toFile();
+            String[] listImg = file.list();
+            if (listImg == null || listImg.length == 0) {
+                System.out.println("There are no elements inside the current folder");
+            } else {
+                for (int j=0;j< listImg.length;j++){
+                    object = new ObjectDetectionModel(searchCriteria,80,pathFolder+"\\"+listImg[j]);
+                    String cadena = String.valueOf(object.getDataList().get(listImg[j].toString()));
+                    matches.add(new MatchInfo(listImg[j],Double.parseDouble(cadena)));
+                    System.out.println(cadena+ "***");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return matches;
     }
 }
