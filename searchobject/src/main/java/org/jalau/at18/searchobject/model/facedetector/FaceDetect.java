4f295b37080a9e48e80b67ea8af167540094125c
Daniela Martinez <daniela.martinez@jala.university>
2022-10-25 23:43:26 -0400

    add the resources of xml and images

--- searchobject/src/main/java/org/jalau/at18/searchobject/model/facedetector/FaceDetect.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/model/facedetector/FaceDetect.java
@@ -18,6 +18,8 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+
 /**
  * It is responsible for analyze an image and detect if there it's a face or not.
  *
@@ -28,17 +30,19 @@ public class FaceDetect {
     private String faceDetection;
     public static void main(String[] args) {
 
-        faceDetection ("images/avengers.jpg", "personal");
+        faceDetection ("D:\\workspacejala\\progra102\\JU-OBJECT-RECOGNITION\\resourcesdetect\\images\\perfil.jpg", "multiple");
 
     }
 
     public static void faceDetection (String file, String type) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        String imgFile = file;//"images/avengers.jpg" //imgFile it's the file we want to analyze if there it's a person in the photo
+        File file2 = new File(file);
+        Boolean detection = false;
+        String imgFile = file2.toString();//"images/avengers.jpg" //imgFile it's the file we want to analyze if there it's a person in the photo
 
         Mat src = Imgcodecs.imread(imgFile);
         // Are the connecting dots of a facial layout. This xml exactly defines a front-face layout
-        String xmlFile = "xml/lbpcascade_frontalface.xml";
+        String xmlFile = "D:\\workspacejala\\progra102\\JU-OBJECT-RECOGNITION\\resourcesdetect\\xml\\lbpcascade_frontalface.xml";
         //cascade classifiers is an effective object detection method
         CascadeClassifier cc = new CascadeClassifier(xmlFile);
         // run a face detector on the imag
@@ -50,26 +54,25 @@ public class FaceDetect {
             for(Rect rect: faceDetection.toArray()) {
                 Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height) , new Scalar(0, 0, 255), 3);
             }
-
+            detection = true;
             Imgcodecs.imwrite("images/facedetect.jpg", src);
             System.out.println("Profile detection");
             System.exit(0);
-        }  else {
-            System.out.println("the image it's wrong, upload the image again");
-            System.exit(0);
-        }
-        if (type == "multiple" && faceDetection.toArray().length > 1) {
+        }  else if (type == "multiple" && faceDetection.toArray().length > 1) {
             for(Rect rect: faceDetection.toArray()) {
                 Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height) , new Scalar(0, 0, 255), 3);
             }
 
             Imgcodecs.imwrite("images/peopledetected.jpg", src);
             System.out.println(String.format("Detected faces: %d", faceDetection.toArray().length));
-            System.out.println("Group detection");
+            detection = true;
             System.exit(0);
         } else {
+            detection = false;
             System.out.println("the image it's wrong, upload the image again");
             System.exit(0);
         }
+
     }
+
 }
