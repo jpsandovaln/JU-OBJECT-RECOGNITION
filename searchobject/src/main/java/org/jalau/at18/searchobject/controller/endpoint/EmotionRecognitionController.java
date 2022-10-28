e5ceea8bb54c87d795846aef4fbb27eb41155580
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-10-28 09:29:55 -0400

    Add EmotionRecognizerException to throw and exception if there is an error with the emotionRecognizer model

--- searchobject/src/main/java/org/jalau/at18/searchobject/controller/endpoint/EmotionRecognitionController.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/controller/endpoint/EmotionRecognitionController.java
@@ -1,7 +1,15 @@
+/**
+ * Copyright (c) 2022 Jala University.
+ *
+ * This software is the confidential and proprieraty information of Jalasoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * Licence agreement you entered into with Jalasoft
+ */
 package org.jalau.at18.searchobject.controller.endpoint;
+import org.jalau.at18.searchobject.common.exception.EmotionRecognizerException;
 import org.jalau.at18.searchobject.controller.service.EmotionRecognitionService;
 import org.jalau.at18.searchobject.controller.service.FilesStorageService;
-import org.jalau.at18.searchobject.model.objectrecognizer.recognizertypes.MatchInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class EmotionRecognitionController{
@@ -23,10 +28,16 @@ public class EmotionRecognitionController{
     EmotionRecognitionService emotionRecognitionService;
     @PostMapping("/emotionRecognition")
     public ResponseEntity <String[]> readData(@RequestParam("file") MultipartFile file,
-                                               @RequestParam String token) throws IOException {
-        Path path = storageService.save(file);
-        String[] data = emotionRecognitionService.processImage(path.toAbsolutePath(), token);
-        return ResponseEntity.status(HttpStatus.OK).body(data);
+                                               @RequestParam String token)  {
+        try {
+            Path path = storageService.save(file);
+            String[] data = emotionRecognitionService.processImage(path.toAbsolutePath(), token);
+            return ResponseEntity.status(HttpStatus.OK).body(data);
+        } catch (EmotionRecognizerException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+
     }
 }
 
--- searchobject/src/main/java/org/jalau/at18/searchobject/controller/service/EmotionRecognitionService.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/controller/service/EmotionRecognitionService.java
@@ -1,18 +1,29 @@
+/**
+ * Copyright (c) 2022 Jala University.
+ *
+ * This software is the confidential and proprieraty information of Jalasoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * Licence agreement you entered into with Jalasoft
+ */
 package org.jalau.at18.searchobject.controller.service;
 
 
+import org.jalau.at18.searchobject.common.exception.EmotionRecognizerException;
 import org.jalau.at18.searchobject.model.emotionrecognizer.EmotionRecognizer;
-import org.jalau.at18.searchobject.model.objectrecognizer.recognizertypes.MatchInfo;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+
+/**
+ *
+ *
+ * @throws EmotionRecognizerException if there is an error reading the file or with the http request
+ */
 
 @Service
 public class EmotionRecognitionService {
-    public String[] processImage(Path pathImage, String token) throws IOException {
+    public String[] processImage(Path pathImage, String token) throws EmotionRecognizerException {
         EmotionRecognizer emotionRecognizer = new EmotionRecognizer(pathImage.toString(), token);
         return emotionRecognizer.getResult();
     }
