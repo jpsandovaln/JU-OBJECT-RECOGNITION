6676719ce1ccc981515c0155e8530b13a6d3a764
Rodrigo Bernal <Rodrigo.Bernal@jala.university>
2022-10-29 17:20:49 -0400

    Add documentation to Response classes

--- searchobject/src/main/java/org/jalau/at18/searchobject/controller/response/ErrorResponse.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/controller/response/ErrorResponse.java
@@ -1,5 +1,19 @@
+/**
+ * Copyright (c) 2022 Jala University.
+ *
+ * This software is the confidential and proprieraty information of Jalasoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * Licence agreement you entered into with Jalasoft
+ */
 package org.jalau.at18.searchobject.controller.response;
 
+/**
+ *
+ *
+ * @author Rodrigo Bernal
+ * @version 1.0
+ */
 public class ErrorResponse {
     private String errorMessage;
     private String status;
