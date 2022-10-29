2de756b0f57dfb18a5e27d1ffb2ad8fa8f6b6ffa
FernandezRobert <soniic9911@gmail.com>
2022-10-29 18:32:58 -0400

    tests

--- searchobject/src/test/java/org/jalau/at18/searchobject/message/ResponseMessageTest.java
+++ searchobject/src/test/java/org/jalau/at18/searchobject/message/ResponseMessageTest.java
@@ -1,5 +1,6 @@
 package org.jalau.at18.searchobject.message;
 
+import org.jalau.at18.searchobject.model.response.ResponseMessage;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
