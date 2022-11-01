7c0c3d2957ee1df20409c4dbe15db546bc24c825
Sarai Alvarez <Sarai.Alvarez@jala.university>
2022-11-01 13:56:16 -0400

    Add documentation to MiddlewareException and GenerateToken class

--- searchobject/src/main/java/org/jalau/at18/searchobject/common/exception/MiddlewareException.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/common/exception/MiddlewareException.java
@@ -1,6 +1,11 @@
 package org.jalau.at18.searchobject.common.exception;
 
-
+/**
+ *Throws exception if a field is empty, if an invalid file is sent, if an invalid token is contained or if no token.
+ *
+ * @author Sarai Alvarez
+ * @version 1.0
+ */
 public class MiddlewareException extends Exception{
 
     public MiddlewareException(String message) {
--- searchobject/src/main/java/org/jalau/at18/searchobject/middleware/token/GenerateToken.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/middleware/token/GenerateToken.java
@@ -1,5 +1,12 @@
 package org.jalau.at18.searchobject.middleware.token;
-
+/**
+ * Copyright (c) 2022 Jala University.
+ *
+ * This software is the confidential and property information of Jalasoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * Licence agreement you entered into with Jalasoft
+ */
 import org.jalau.at18.searchobject.common.logger.At18Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +18,18 @@ import java.util.Scanner;
 import java.util.UUID;
 import java.util.function.Supplier;
 import java.util.logging.Logger;
-
+/**
+ * This class is to generate the token
+ * @author Sarai Alvarez
+ * @version 1.0
+ */
 @RestController
 public class GenerateToken {
     private static final Logger LOG = new At18Logger().getLogger();
-
+    /**
+     *Each StringBuilder object is capable of storing different characters specified by its capacity. If the capacity
+     *of a StringBuilder object is exceeded, it automatically expands to accommodate the additional characters.
+     */
     @GetMapping("/getToken")
     public StringBuilder generateToken() throws IOException {
         String Path = System.getProperty("user.dir") + "\\src\\main\\java\\org\\jalau\\at18\\searchobject\\middleware\\token\\token.txt";
