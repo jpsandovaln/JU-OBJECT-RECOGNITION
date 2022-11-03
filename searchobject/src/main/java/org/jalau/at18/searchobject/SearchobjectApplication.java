e7285842e24ca427bf3723de7a47bf285d869ca7
Daniela Martinez <daniela.martinez@jala.university>
2022-11-02 23:09:32 -0400

    document the package notify

--- searchobject/src/main/java/org/jalau/at18/searchobject/SearchobjectApplication.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/SearchobjectApplication.java
@@ -1,5 +1,12 @@
 package org.jalau.at18.searchobject;
-
+/**
+ * Copyright (c) 2022 Jala University.
+ *
+ * This software is the confidential and proprietary information of Jala University
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jala University
+ */
 import org.jalau.at18.searchobject.controller.service.FilesStorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
--- searchobject/src/main/java/org/jalau/at18/searchobject/notify/VerifyNotifier.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/notify/VerifyNotifier.java
@@ -9,13 +9,17 @@
 package org.jalau.at18.searchobject.notify;
 
 import org.jalau.at18.searchobject.common.exception.NotifierTypeException;
-
 /**
- *
- *
- * @throws NotifierTypeException if the notifier type is not one of the available notifiers
+ * Send notifications of the result according to the request to phone numbers.
+ * @author Maria Hurtado
+ * @version 1.0
  */
 public class VerifyNotifier {
+    /**
+     * method to get the notification in the cellphone number in whatsapp
+     * @param notifierType type of notification
+     * @throws NotifierTypeException if the notifier type is not one of the available notifiers
+     */
     public Notifier getNotifier(String notifierType) throws NotifierTypeException {
         if(notifierType.equals(TypeNotifier.WHATSAPP.getNotifier())) {
             return new NotifierWhatsApp();
