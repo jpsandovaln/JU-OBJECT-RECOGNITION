da787d899eb75bb69d089a23472c298b522cca7c
Daniela Martinez <daniela.martinez@jala.university>
2022-11-03 21:12:16 -0400

    last documentation of the classes

--- searchobject/src/main/java/org/jalau/at18/searchobject/controller/endpoint/EmotionRecognitionController.java
+++ searchobject/src/main/java/org/jalau/at18/searchobject/controller/endpoint/EmotionRecognitionController.java
@@ -21,7 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Path;
 /**
- *
+ *Endpoint of the service of the API the google for emotion recognition
  *
  * @author Alvaro Sivila
  * @version 1.0
@@ -29,15 +29,23 @@ import java.nio.file.Path;
 @RestController
 public class EmotionRecognitionController{
     @Autowired
-    FilesStorageService storageService;
+    FilesStorageService storageService;  //storage file
     @Autowired
-    EmotionRecognitionService emotionRecognitionService;
+    EmotionRecognitionService emotionRecognitionService;  //emotion recognition service
+    /**
+     *Method to take the data from the data send for postman
+     * @param file upload image for analyze
+     * @param token access to the service
+     * @throws EmotionRecognizerException if the file or the token it's incorrect and can't continue with the process
+     */
     @PostMapping("/emotionRecognition")
     public ResponseEntity readData(@RequestParam("file") MultipartFile file,
                                                @RequestParam String token)  {
         try {
-            Path path = storageService.save(file);
+            Path path = storageService.save(file);//save file
+            //process the image to be analyze
             String[] data = emotionRecognitionService.processImage(path.toAbsolutePath(), token);
+            //return the result
             return ResponseEntity.status(HttpStatus.OK).body(data);
         } catch (EmotionRecognizerException e) {
             return ResponseEntity.badRequest().body(new ErrorResponse("400",e.getMessage()));
